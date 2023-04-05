53a6ef93654e8705dca751eadc571fdba7cff8da
Luojia_onlyone <104088291+Luojianolyone@users.noreply.github.com>
2023-04-05 23:04:11 +0800

    Add files via upload
    获取所有基金代码、类型、简写。写入本地数据库，当前目录也写入一遍csv文件

---
+++ fundt.py
@@ -0,0 +1,23 @@
+import json
+import re
+
+import requests
+import sqlalchemy
+import pandas as pd
+
+
+if __name__ == '__main__':
+    # 建立连接
+    engine = sqlalchemy.create_engine("mysql+pymysql://root:luo3306.J@localhost:3306/fund")
+
+
+    url = 'http://fund.eastmoney.com/js/fundcode_search.js'
+    r = requests.get(url)
+    cont = re.findall('var r = (.*])', r.text)[0]  # 提取list
+    ls = json.loads(cont)  # 将字符串个事的list转化为list格式
+    fund = pd.DataFrame(ls, columns=['fundcode', 'fundsx', 'name', 'category', 'fundpy'])  # list转为DataFrame
+    fund.to_sql('fund_all',con=engine,index=False,if_exists='replace')
+    fund.to_csv("fund_all.csv",index=False,encoding='gbk')
+    print('写入成功！！！')
+
+
